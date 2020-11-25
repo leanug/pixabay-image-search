fe24ede3f1493f586331148ddd5745817ebe4b9b
Leandro Ubilla Gonzalez <46423097+leanug@users.noreply.github.com>
2020-11-24 21:52:18 -0300

    Commit 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React, { Fragment, useState } from 'react';
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <Fragment>
+      <Formulario />
+    </Fragment>
+  );
+}
+
+export default App;
